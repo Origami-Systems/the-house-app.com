1297b253444d09b8e56fb0929295d71397a1d169
OrigamiKing3612 <origamiking3612@gmail.com>
2026-05-29 20:12:52 -0500

    update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 
 import vue from "@astrojs/vue";
 import { defineConfig } from "astro/config";
+import { satteri } from '@astrojs/markdown-satteri';
 
 import sitemap from "@astrojs/sitemap";
 
@@ -10,6 +11,11 @@ import mdx from "@astrojs/mdx";
 export default defineConfig({
     site: "https://the-house-app.com",
     output: "static",
+    markdown: {
+        processor: satteri({
+            features: { directive: true },
+        }),
+    },
     integrations: [vue(), sitemap(), mdx()],
     experimental: {
         contentIntellisense: true,
